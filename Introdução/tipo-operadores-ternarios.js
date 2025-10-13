ca09404903971923cb0a2f9d9414758a5406c271
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-13 13:58:45 -0300

    add exercício de op ternários

---
+++ Introdução/tipo-operadores-ternarios.js
@@ -0,0 +1,13 @@
+// 🟢 Fácil
+
+// Verifique se uma pessoa é maior ou menor de idade.
+
+// Diga se um número é par ou ímpar.
+
+let idade = 17;
+let verificacao = idade >= 18 ? 'Maior de Idade' : 'Menor de Idade';
+console.log(verificacao);
+
+let numero = 4;
+let validacaoDeNumero = numero % 2 === 0 ? 'Par' : 'Ímpar';
+console.log(validacaoDeNumero);
