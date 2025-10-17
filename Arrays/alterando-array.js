fa9a71558dc4b25795f20db27cf974d1c84c10fd
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-17 16:01:07 -0300

    concatarrays

--- Arrays/alterando-array.js
+++ Arrays/alterando-array.js
@@ -1,4 +1,4 @@
 const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
 
-listaEstudantes.splice(1, 2, 'Rdorigo',);
+listaEstudantes.splice(1, 2, 'Rdorigo',); // A partir do indìce 1 ele remove 2 dados e add Rodrigo nó indíce em que começa
 console.log(listaEstudantes);
---
+++ Arrays/concatenando.arrays.js
@@ -0,0 +1,6 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Pedro', 'Allicia', 'Leo'];
+
+const juncaoDasTurmas = salaJS.concat(salaPython);
+console.log(juncaoDasTurmas);
+
