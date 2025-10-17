69eedb39b4cf93b4076b1b204d110826e5ff41a8
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-17 14:17:01 -0300

    alterado para arrowfunctiond

--- Projects/lista-de-compras.js
+++ Projects/lista-de-compras.js
@@ -1,44 +1,39 @@
 let entradaConfirmada = true;
 
 if (entradaConfirmada) {
-    function boasVindas() {
+    const boasVindas = () => {
         console.log('Boas Vindas ao Seu Gerenciador de Compras!');
-    }
+    };
 
-    function gerarLista() {
+    const gerarLista = () => {
         let lista = [];
         console.log(`Lista gerada (Lista Vazia): ${lista}`);
 
-        function addElementoLista() {
+        const addElementoLista = () => {
             lista.push('Pedro', 123, 'alli', 'leo', 222);
+        };
 
-        }
-
-        function elementosAdicionados() {
+        const elementosAdicionados = () => {
             console.log(`Itens adicionados: ${lista}`);
-        }
+        };
 
-        function removeItens() {
+        const removeItens = () => {
             lista.splice(0, 1);
-        }
+        };
 
-
-        function exibirLista() {
+        const exibirLista = () => {
             console.log(`Lista pós alterações: ${lista}`);
-        }
+        };
 
         addElementoLista();
         elementosAdicionados();
         removeItens();
         exibirLista();
-
-    }
+    };
 
     boasVindas();
     gerarLista();
 
 } else {
-    console.log('Sua entrada não foi válidada!')
+    console.log('Sua entrada não foi válida!');
 }
-
-
