37f146a031a52a7bef70d4a6f22f3fa90b7f49d9
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-20 17:15:54 -0300

    Atividade Importante

--- For/for-basico.js
+++ For/for-basico.js
@@ -14,11 +14,31 @@
 
 // Crie uma função que receba uma array de números inteiros e retorne a soma dos elementos.
 
-let soma = 0;
+// let soma = 0;
 
-const numeros = [2, 4, 6, 8, 10];
-for (let i = 0; i < numeros.length; i++) {
-    soma += numeros[i];
+// const numeros = [2, 4, 6, 8, 10];
+// for (let i = 0; i < numeros.length; i++) {
+//     soma += numeros[i];
+// }
+
+// console.log(soma);
+
+// Crie uma função que receba uma array de números inteiros
+// e retorne o menor valor e o maior valor da array, no seguinte formato: 'o menor número é X e o maior número é Y'.
+
+const numerosInteiros = [2, 4, 6, 8, 10, 12];
+
+let maior = numerosInteiros[0];
+let menor = numerosInteiros[0];
+
+for (let i = 0; i < numerosInteiros.length; i++) {
+    if (numerosInteiros[i] < menor) {
+        menor = numerosInteiros[i];
+    }
+    if (numerosInteiros[i] > maior) {
+        maior = numerosInteiros[i];
+    }
 }
 
-console.log(soma);
+console.log(`O menor número é o: ${menor}`);
+console.log(`O maior número é o: ${maior}`);
